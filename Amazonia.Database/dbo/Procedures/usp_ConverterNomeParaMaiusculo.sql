f38ba761325cb124836f4d39812e755c606e26a2
Ricardo Reis <ricardo190484@gmail.com>
2021-11-11 16:58:55 +0000

    Ligacao a Base de Dados Local

---
+++ Amazonia.Database/dbo/Procedures/usp_ConverterNomeParaMaiusculo.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[usp_ConverterNomeParaMaiusculo]
+	@param1 VARCHAR(50) = 0     --Parâmetro de Entrada
+AS
+	SELECT UPPER(@param1)
+RETURN 0
---
+++ Amazonia.Database/dbo/Tables/Telefone.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Telefone] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [NumeroTelefone] VARCHAR (20) NULL,
+    [AlunoId]        INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([AlunoId]) REFERENCES [dbo].[Aluno] ([Id])
+);
+
