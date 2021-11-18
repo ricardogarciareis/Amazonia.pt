176a0032623bac9a9b9b53205db3a9e262baba0d
Ricardo Reis <ricardo190484@gmail.com>
2021-11-18 19:46:01 +0000

    Banco SQLServer criado e testado

--- Amazonia.ConsoleAPP/Program.cs
+++ Amazonia.ConsoleAPP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using Amazonia.DAL.Modelo;
 using Amazonia.DAL.Repositorios;
 
 namespace Amazonia.ConsoleAPP
@@ -8,6 +9,18 @@ namespace Amazonia.ConsoleAPP
     {
         static void Main(string[] args)
         {
+            var ctx = new AmazoniaContexto();
+            ctx.Clientes.Add(new Cliente
+            {
+                UserName = "joao.silva",
+                DataNascimento = new DateTime(1984, 05, 29),
+                Nome = "João da Silva",
+                NumeroIdentificacaoFiscal = "123456789",
+                Password = "senha"
+            });
+            ctx.SaveChanges();
+
+
             //var chaveExemploDireto = ConfigurationManager.AppSettings["chaveExemplo"]       Opção 1
             //Console.WriteLine(chaveExemploDireto)
             //var valorObtidoPeloMetodo = AcessoAppConfig.ObterValorDoConfig("chaveExemplo")  Opção 2
--- Amazonia.DAL/Modelo/AmazoniaContexto.cs
+++ Amazonia.DAL/Modelo/AmazoniaContexto.cs
@@ -6,5 +6,23 @@ namespace Amazonia.DAL.Modelo
     {
         public DbSet<Morada> Moradas { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //=> options.UseSqlServer(@$"Server=(LocalDB)\MSSQLLocalDB;Database=Amazonia.pt;Trusted_Connection=True;");
+
+        //Exemplo SQLServer
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = @$"Server=(LocalDB)\MSSQLLocalDB;Database=Amazonia.pt;Trusted_Connection=True;";
+            options.UseSqlServer(connectionString);
+        }
+
+        //Exemplo SQLite
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    var connectionString = @$"Data Source=c:\temp\meudb.db";
+        //    options.UseSqlite(connectionString);
+        //}
+
     }
 }
