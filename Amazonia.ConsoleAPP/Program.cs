f7cb0dcd63fca4b0784e33f783023149eb76262f
Ricardo Reis <ricardo190484@gmail.com>
2021-11-20 12:56:28 +0000

    #Inicio Linq

--- Amazonia.ConsoleAPP/Program.cs
+++ Amazonia.ConsoleAPP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using Amazonia.DAL.Modelo;
@@ -13,6 +14,13 @@ namespace Amazonia.ConsoleAPP
         {
             var ctx = new AmazoniaContexto();
 
+            PlaygroundLinq(ctx);
+
+
+
+
+            //GuardCondition
+            return;
 
             //CREATE
             //AdicionarClienteExemplo(ctx);
@@ -57,6 +65,198 @@ namespace Amazonia.ConsoleAPP
             }
             
 
+        }
+
+        private static void PlaygroundLinq(AmazoniaContexto ctx)
+        {
+            //SelecaoSimples(ctx);
+            //SelecaoComPaginacao(ctx);
+            //SelecacoComOrdenacao(ctx);
+            //CriacaoNovosClientes(ctx);
+            //DadosEspecificosDeClientes(ctx);
+            //SQLRaw(ctx);
+            //ProjecaoComJuncao(ctx);
+
+            var clientesMoramPorto = ctx.Clientes.Where(x => x.Morada.Localidade == "Porto").ToList();
+            //LazyLoad
+            var clientesMoramPortoComOutrasMoradas = ctx.Clientes.Include("Morada").Where(x => x.Morada.Localidade == "Porto").ToList();
+
+            foreach (var i in clientesMoramPortoComOutrasMoradas)
+            {
+                Console.WriteLine("Nome: " + i.Nome);
+                Console.WriteLine("Endereço: " + i.Morada.Endereco);
+                Console.WriteLine("--------------------------------------------");
+            }
+
+
+
+            //throw new NotImplementedException();
+        }
+
+        private static void SQLRaw(AmazoniaContexto ctx)
+        {
+            //BUG:
+            var clientesQueMoramPortoSQLRaw = ctx.Clientes
+                .FromSqlRaw("SELECT c.Nome, m.Endereco " +
+                            "FROM Clientes c " +
+                            "LEFT JOIN Moradas m ON c.MoradaId = m.Id " +
+                            "WHERE m.Localidade = 'PORTO'").ToList();
+
+            foreach (var i in clientesQueMoramPortoSQLRaw)
+            {
+                Console.WriteLine("Nome: " + i.Nome);
+                Console.WriteLine("Endereço: " + i.Morada.Endereco);
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+
+        private static void ProjecaoComJuncao(AmazoniaContexto ctx)
+        {
+            var clientesQueMoramNoPorto = from c in ctx.Clientes
+                                          join m in ctx.Moradas
+                                          on c.Morada.Id equals m.Id
+                                          where m.Localidade == "Porto"
+                                          select new
+                                          {
+                                              c.Nome,
+                                              m.Endereco
+                                          };
+
+            var clientesQueMoramNoPortoFluentAPI = ctx.Clientes
+                .Where(x => x.Morada.Localidade == "Porto")
+                .Select(x => new
+                {
+                    Nome = x.Nome,
+                    Endereco = x.Morada.Endereco
+                });
+
+
+            foreach (var i in clientesQueMoramNoPortoFluentAPI)
+            {
+                Console.WriteLine("Nome: " + i.Nome);
+                Console.WriteLine("Endereço: " + i.Endereco);
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+
+        private static void DadosEspecificosDeClientes(AmazoniaContexto ctx)
+        {
+            //SELECT * FROM clientes c LEFT JOIN moradas m ON m.id = c.morada.id WHERE m.Distrito = 'Porto'
+
+            var clientesQueMoramNoPorto = ctx.Clientes.Where(x => x.Morada.Distrito == "Porto").ToList();
+            //-------------------------------------------------------------------------------------------
+            var dadosEspecificos = clientesQueMoramNoPorto.Select(x => new
+            {
+                x.Nome,
+                //x.Morada.Endereco,           não acede
+                x.NumeroIdentificacaoFiscal
+            });
+            foreach (var i in dadosEspecificos)
+            {
+                Console.WriteLine(i);
+            }
+            //-------------------------------------------------------------------------------------------
+            var dadosEspecificosModificados = clientesQueMoramNoPorto.Select(x => new
+            {
+                NomeEmMaiusculo = x.Nome.ToUpper(),
+                NIF = x.NumeroIdentificacaoFiscal
+            });
+            foreach (var i in dadosEspecificosModificados)
+            {
+                Console.WriteLine(i);
+            }
+            //-------------------------------------------------------------------------------------------
+        }
+
+        private static void CriacaoNovosClientes(AmazoniaContexto ctx)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var clienteNovo = new Cliente
+                {
+                    DataNascimento = new DateTime(1984, i + 1, i + 1),
+                    Nome = "João da Silva" + i,
+                    NumeroIdentificacaoFiscal = "123456789",
+                    UserName = "joao.silva" + i,
+                    Password = "abc,1234",
+                    Morada = new Morada
+                    {
+                        CodigoPostal = "123456" + i,
+                        Distrito = "Porto",
+                        Localidade = "Porto",
+                        Endereco = "Rua das Casas no " + (i + 1),
+                        Nome = "Morada Principal"
+                    }
+                };
+                ctx.Clientes.Add(clienteNovo);
+            }
+            ctx.SaveChanges();
+        }
+
+        private static void SelecacoComOrdenacao(AmazoniaContexto ctx)
+        {
+            var listaNomes = new List<string> { "João", "Mateus", "Marcos", "Lucas", "Maria", "Pedro" };
+            var nomesOrdenadosAsc = listaNomes.OrderBy(x => x);
+            var nomesOrdenadosDesc = listaNomes.OrderByDescending(x => x);
+
+            //SELECT * FROM livros ORDER BY preco
+            var livrosMaisBaratosPrimeiro = ctx.Livros.OrderBy(x => x.ObterPreco);
+
+            //SELECT * FROM livros ORDER BY nome, preco
+            var livrosOrderPrecoENome = ctx.Livros.OrderBy(x => x.Nome).ThenByDescending(x => x.ObterPreco);
+        }
+
+        private static void SelecaoComPaginacao(AmazoniaContexto ctx)
+        {
+            //Selecionar primeiro livro e transformar NOME em UPPERCASE
+            var primeiroLivro = ctx.Livros.FirstOrDefault(x => x.Idioma == DAL.Idioma.Portugues);
+            var primeiroLivroNome = primeiroLivro.Nome.ToUpper();
+
+            //Selecionar primeiros 10 livros (paginação)
+            var primeiros10Livros = ctx.Livros.Take(10).Where(x => x.Idioma == DAL.Idioma.Portugues);
+
+            //Selecionar primeiros 10 livros com skip (paginação)
+            var primeiros10LivrosEscape10Iniciais = ctx.Livros.Skip(10).Take(10).Where(x => x.Idioma == DAL.Idioma.Portugues);
+        }
+
+        private static void SelecaoSimples(AmazoniaContexto ctx)
+        {
+            //Selecionar um objeto com Linq SELECT
+            //Select * From tabela
+
+            var listaResultado = new List<Livro>();
+            foreach (var livro in ctx.Livros)
+            {
+                if (livro.Nome.StartsWith("H"))
+                {
+                    listaResultado.Add(livro);
+                }
+            }
+            //Substituído por:
+            //Linq antigo
+            var livrosX = from livro in ctx.Livros
+                          where livro.Nome.StartsWith("H")
+                          select livro;
+
+            //Com SQL
+            //SELECT * FROM livros WHERE nome LIKE 'h%'
+
+            //Linq com FluentAPI
+            var livros = ctx.Livros.Where(l => l.Nome.StartsWith("H")).ToList();
+            foreach (var livro in livros)
+            {
+                Console.WriteLine(livro);
+            }
+
+            //Com OU
+            var senhorDosAneisOuHarryPotter = ctx.Livros.Where(l => l.Nome.StartsWith("h") || l.Nome.StartsWith("l")).ToList();
+
+            //Exemplo Quebra de Linha
+            var senhorDosAneisOuHarryPotter2 = ctx.Livros
+                .Where(l =>
+                    l.Nome.StartsWith("h")
+                    || l.Nome.StartsWith("l"))
+                .ToList();
         }
 
         private static void ImprimirValorBaseadoEmConfiguracao()
